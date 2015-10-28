2603bbf6005ec2a4debbba7c70d8d26ecc5e4aae
just-barcodes <justbarcodes@gmail.com>
2015-10-28 18:47:57 +0000

    google calendar include update

--- go_cal_we_color.js
+++ go_cal_we_color.js
@@ -2,6 +2,7 @@
 // @name        google cal weekend color
 // @namespace   https://www.google.com/calendar/
 // @include     https://www.google.com/calendar/*
+// @include     https://calendar.google.com/calendar/*
 // @description adds a redish color to weekends on google calendar
 // @version     1
 // @grant       none
